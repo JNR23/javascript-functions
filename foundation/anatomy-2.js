3d2a41b3d9c91fbc1352723f5f9ca0e98e417890
JNR23 <junior_bhzte@hotmail.com>
2020-12-08 23:41:28 -0300

    Function expression

---
+++ foundation/anatomy-2.js
@@ -0,0 +1,17 @@
+//Anonymous function
+
+(function (a, b, c) {
+    return a + b + c
+})
+
+//Function expression
+
+const sum = function (a, b) {
+    return a + b
+}
+
+const result = sum(7, 59)
+console.log(result)
+
+const anotherSum = sum
+console.log(anotherSum(4, 4))
